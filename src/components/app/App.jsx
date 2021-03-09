14ff680c24fd6d55a0118a3788cd8b5017725c64
Kateryna Tytova <titova.kat.85@gmail.com>
2021-03-09 14:26:31 +0200

    feat: add main and country pages structure

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Element from '../block-one';
-import ShowplaceService from '../../services';
 import MainPage from '../main-page';
 import CountryPage from '../country-page';
 
 function App() {
-  const showplacesService = new ShowplaceService();
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    showplacesService.getCountry('italy').then((res) => setData(res));
-  }, []);
   return (
     <Router>
       <Switch>
@@ -23,10 +16,6 @@ function App() {
           <CountryPage />
         </Route>
       </Switch>
-
-      {/* <div>
-        <Element data={data} />
-      </div> */}
     </Router>
   );
 }
