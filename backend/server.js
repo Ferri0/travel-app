dfd38a1b846ad044cbe3283ee75a9f10efb96065
Denis <DenisOleksiuk95@gmail.com>
2021-03-10 22:49:32 +0200

    refactor: delete block one component

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 // const countries = require('./countries/data-of-countries');
-let countries =[];
+let countries = [];
 const Country = require('./countries/countries.schema');
 
 const mongoURL = 'mongodb+srv://IgorAleks88:Veremiy1988@cluster0.abmvg.mongodb.net/travel-app?retryWrites=true&w=majority';
@@ -14,28 +14,15 @@ app.get('/api/countries', (req, res) => {
   res.send(JSON.stringify(countries));
 });
 
-app.get('/api/countries/england', (req, res) => {
-  const england = countries.find((country) => country.england)
-  res.send(JSON.stringify(england));
-});
-
-app.get('/api/countries/italy', (req, res) => {
-  const england = countries.find((country) => country.italy)
-  res.send(JSON.stringify(england));
-});
-
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-      }).then(() => {
-        app.listen(PORT);
-        Country.find({}).then(res => {
-          countries = res;
-          // console.log(countries);
-        });
-       
-      })
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).then(() => {
+  app.listen(PORT);
+  Country.find({}).then(res => {
+    countries = res;
+  });
+})
--- src/components/country-page/country-page-content/country-page-content.jsx
+++ src/components/country-page/country-page-content/country-page-content.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import style from './country-page-content.module.scss';
-import { Element } from '../../block-one';
 
 function CountryPageContent() {
   const location = useLocation();
   const country = location.propsCountry;
-  const attractions = location.propsAttractions;
-
+  // const attractions = location.propsAttractions;
+  console.log(country);
   return (
     <div className={style.wrapper}>
       <Link to="/">Back to Main Page</Link>
       <div className={style.countryTitle}>{country}</div>
-      <div>
-        <Element showplaces={attractions} />
-      </div>
     </div>
   );
 }
