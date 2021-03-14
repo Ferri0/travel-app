4a27e4c158b867058e5b5b4759959a313a6dc703
Kateryna Tytova <titova.kat.85@gmail.com>
2021-03-14 20:01:13 +0200

    feat: add search styles, add search focus on download app, add clear button, add search button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,23 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import style from './header.module.scss';
-
-function getSearch(parrent) {
-  if (parrent === 'countryPage') return false;
-  return (
-    <input
-      type="search"
-      className={style.searchInput}
-      placeholder="search country"
-    />
-  );
-}
+import { Search } from './search';
 
 function Header({ parrent }) {
+  // const inputRef = useRef(null);
+
+  // useEffect(() => {
+  //   inputRef.current.focus();
+  // }, []);
+
+  // function getSearch(cls) {
+  //   if (cls === 'countryPage') return false;
+  //   return (
+  //     <div>
+  //       <input
+  //         ref={inputRef}
+  //         type="search"
+  //         className={style.searchInput}
+  //         placeholder="search country"
+  //         results={0}
+  //       />
+  //       <button type="button" className={style.searchButton}>
+  //         <i className="fas fa-search" />
+  //       </button>
+  //     </div>
+  //   );
+  // }
+
   return (
     <div className={style.header}>
       <Link to="/" className={style.logo} />
-      {getSearch(parrent)}
+      <Search parrent={parrent} />
       Localization
     </div>
   );
