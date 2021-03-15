890d25db79f1a4796889dea5c6ea622b50d9fbdb
Denis <DenisOleksiuk95@gmail.com>
2021-03-16 01:43:06 +0200

    eh: qw: command not found

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import style from './slider.module.scss';
 import { VControl } from './v-control';
