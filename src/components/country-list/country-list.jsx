72065be9ec42c54d02f55fac12e0ae2a860cd031
Kateryna Tytova <titova.kat.85@gmail.com>
2021-03-16 15:56:11 +0200

    feat: add rating into gallery block

--- src/components/country-list/country-list.jsx
+++ src/components/country-list/country-list.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
-import { ShowplaceService } from '../../services';
-import { setShowAuth } from '../../action';
 import { Widgets } from '../widgets';
 import { Gallery } from './gallery';
 import RatingPage from '../rating-page';
 import style from './country-list.module.scss';
 
-const showplaceService = new ShowplaceService();
-
 const CountryList = (props) => {
-  const { currentUser, currentCounrty, lang, setShowAuthAction } = props;
+  const { currentCounrty, lang } = props;
   const {
     name_lang: title,
     attraction,
@@ -51,43 +47,12 @@ const CountryList = (props) => {
       <div className={style.video}>
         <ReactPlayer url={video} />
       </div>
-      <Gallery attraction={attraction} lang={lang} />
+      <Gallery
+        attraction={attraction}
+        lang={lang}
+        currentCounrty={currentCounrty}
+      />
 
-      {/* <ul className="countri__list">
-        {attraction.map(({ name, img, description, id, rate }, index) => (
-          <li key={id}>
-            <div>
-              <b>{name[lang]}</b>
-            </div>
-            <img width="250px" src={img} alt={name} />
-            <div className="rating-buttons-wrapper">
-              {[1, 2, 3, 4, 5].map((i) => (
-                <button
-                  key={`${_id}${i}`}
-                  onClick={() => {
-                    if (currentUser) {
-                      showplaceService.rate(_id, index, currentUser, i);
-                    } else {
-                      setShowAuthAction(true);
-                    }
-                  }}
-                  type="button"
-                >
-                  {i}
-                </button>
-              ))}
-            </div>
-            <div className="rating-marks-wrapper">
-              {rate.map((mark, i) => (
-                <span key={i}>{`${mark.user}: ${mark.rating}`}</span>
-              ))}
-            </div>
-            <div>
-              <em>{description[lang]}</em>
-            </div>
-          </li>
-        ))}
-      </ul> */}
       {/* import { Widgets } from '../widgets';
 import RatingPage from '../rating-page';
 
