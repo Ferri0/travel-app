5c5b1786f39cd68bfbf7a578413fff5e6acc634d
Denis <DenisOleksiuk95@gmail.com>
2021-03-16 16:58:01 +0200

    refactor rating to a different component

--- src/components/country-list/country-list.jsx
+++ src/components/country-list/country-list.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+<<<<<<< HEAD
 import ReactPlayer from 'react-player';
 import { setShowAuth } from '../../action';
 import { Widgets } from '../widgets';
@@ -84,12 +84,41 @@ const CountryList = (props) => {
           </li>
         ))}
       </ul> */}
+=======
+import { Widgets } from '../widgets';
+import RatingPage from '../rating-page';
+
+import './country-list.scss';
+
+const CountryList = (props) => {
+  const { currentCounrty, lang } = props;
+  const { name_lang: title, attraction, img: catipalImg  } = currentCounrty;
+    
+  return (
+    <div>
+      <h2 className="countri__title">{ title[lang] }</h2>
+      <img className="country__img" src={catipalImg} alt="capital-img"/>
+      <Widgets />
+      <ul className="countri__list">
+        {
+          attraction.map(({ name, img, description, id, rate}, index) => (
+              <li key={id}>
+                <div><b>{name[lang]}</b></div>
+                <img width="250px" src={img} alt={name}/>
+                <RatingPage rate={rate} index={index} currentCounrty={currentCounrty}/>
+                <div><em>{description[lang]}</em></div>
+              </li>
+          ))
+        }
+      </ul>
+>>>>>>> 6049b91... refactor rating to a different component
     </div>
   );
 };
 
 CountryList.propTypes = {
   currentCounrty: PropTypes.objectOf(PropTypes.any).isRequired,
+<<<<<<< HEAD
   lang: PropTypes.string.isRequired,
 };
 
@@ -103,3 +132,11 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CountryList);
+=======
+  lang: PropTypes.string.isRequired
+}
+
+
+export default CountryList;
+
+>>>>>>> 6049b91... refactor rating to a different component
