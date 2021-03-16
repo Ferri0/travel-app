81283d481f27c6c86932191b5170ef19654db997
Denis <DenisOleksiuk95@gmail.com>
2021-03-16 16:56:42 +0200

    feat: add marks-list update

--- src/components/country-list/country-list.jsx
+++ src/components/country-list/country-list.jsx
@@ -24,7 +24,6 @@ const CountryList = (props) => {
   };
 
   return (
-<<<<<<< HEAD
     <div className={style.countryContent}>
       <div
         className={style.countryDescription}
@@ -85,43 +84,6 @@ const CountryList = (props) => {
           </li>
         ))}
       </ul> */}
-=======
-    <div>
-      <h2 className="countri__title">{ title[lang] }</h2>
-      <img className="country__img" src={catipalImg} alt="capital-img"/>
-      <Widgets />
-      <ul className="countri__list">
-        {
-          attraction.map(({ name, img, description, id, rate}, index) => (
-              <li key={id}>
-                <div><b>{name[lang]}</b></div>
-                <img width="250px" src={img} alt={name}/>
-                <div className = "rating-buttons-wrapper">
-                {[1,2,3,4,5].map((i) => (
-                    <button key = {`${_id}${i}`}
-                    onClick = {() => {
-                      if (currentUser) {
-                      showplaceService.rate(_id, index, currentUser, i).then(() => { 
-                        fetchShowplaceAction(showplaceService);
-                      });
-                      } else {
-                        setShowAuthAction(true);
-                      }
-                    }}
-                    type="button">{i}</button>
-                  ))}
-                </div>
-                <div className = "rating-marks-wrapper">
-                  {rate.map((mark) => (
-                    <span>{`${mark.user}: ${mark.rating}`}</span>
-          ))}
-                </div>
-                <div><em>{description[lang]}</em></div>
-              </li>
-          ))
-        }
-      </ul>
->>>>>>> 4107d45... add marks-list update
     </div>
   );
 };
